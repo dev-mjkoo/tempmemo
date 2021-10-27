07f6dedaba68727c191969501f71115d4e2d09f2
KooMinJun <mjkoo711@gmail.com>
2021-10-27 21:24:21 +0900

    init setting

---
+++ tempmemo/tempmemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  tempmemoApp.swift
+//  tempmemo
+//
+//  Created by 구민준 on 2021/10/27.
+//
+
+import SwiftUI
+
+@main
+struct tempmemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
